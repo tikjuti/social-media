c340dbc9b58e80666d53149ec97bfce56c6badf2
tikjuti <tangquoctuan2003@gmail.com>
2024-09-16 22:08:53 +0700

    add friend admin

--- friends/admin.py
+++ friends/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Friend, FriendRequest
 
 # Register your models here.
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_editable = ['status']
+    list_display = ['sender', 'receiver', 'status']
+
+class FriendAdmin(admin.ModelAdmin):
+    list_display = ['user', 'friend']
+    
+admin.site.register(Friend, FriendAdmin)
+admin.site.register(FriendRequest, FriendRequestAdmin)
